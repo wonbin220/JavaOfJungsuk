eff35f88369e5aea3b39c2882a806eac1d674a66
wonbin220 <wonbin220@gmail.com>
2019-01-12 03:19:30 +0900

    ch04 ex11까지

---
+++ ch04/src/FlowEx3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FlowEx3 {
+
+	public static void main(String[] args) {		
+		System.out.print("숫자를 하나 입력하세요.>");
+		
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt(); // 화면을 통해 입력받은 숫자를 input에 저장
+		
+		if(input == 0) {
+			System.out.println("입력하신 숫자는 0입니다.");
+		}else {
+			System.out.println("입력하시는 숫자는 0이 아닙니다.");
+		}
+	}//메인의 끝
+
+}
